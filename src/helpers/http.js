3b2b76514a3b4c6adc470f7f62dc531ea1370606
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-03-16 21:29:56 -0300

    implement delete and update methods in http

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,16 +1,30 @@
 const baseUrl = 'https://meetus-5dafa-default-rtdb.firebaseio.com/'
 const get = (resorce, headers = {}) => {
-  return fetch(`${baseUrl}/${resorce}.json`, {headers, method: 'GET'});
+  return fetch(getUrl(resorce), { method: 'GET', headers});
 }
 
-const post = (resorce, body, headers = {'Content-Type': 'application/json'}) => {
-  return fetch(`${baseUrl}${resorce}.json`, {method: 'POST', headers, body: JSON.stringify(body)});
+const getUrl = (resource) => `${baseUrl}${resource}.json`;
+
+const post = (resource, body, headers = {'Content-Type': 'application/json'}) => {
+  return fetch(getUrl(resource), { method: 'POST', headers, body: JSON.stringify(body)});
 }
+
+const put = (resource, id, body, headers = {'Content-Type': 'application/json'}) => {
+  return fetch(getUrl(`${resource}/${id}`), { method: 'PUT', headers, body: JSON.stringify(body)});
+}
+
+
+const deleteMeetup = (resource, id, headers = {'Content-Type': 'application/json'}) => {
+  return fetch(getUrl(`${resource}/${id}`), { method: 'DELETE', headers });
+}
+
 
 const http = {
   baseUrl,
   get,
-  post
+  post,
+  put,
+  delete: deleteMeetup
 }
 
 export default http;
